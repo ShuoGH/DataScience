e81e79131da24fa01c6d6ea066ee66a371a0e87f
Shuo <anshuobuaa@gmail.com>
2018-11-02 12:21:22 +0000

    the cw1 R

--- foundationds/cw1-fish.R
+++ foundationds/cw1-fish.R
@@ -6,8 +6,15 @@
 fish <- read.table("fish1.txt")
 summary(fish)
 str(fish)
-
+#1. draw the plots 
 #draw the scatter plot 
 attach(fish)
 plot(V1,V2,main = "scatterplot",xlab = "time",ylab = "size",col = c("red","green","blue")[fish$V3]) #plot with different color 
 #want to make different x y axis
+
+#the bait and the size of the fish
+plot(V3,V2,main="the bait and the fish size",xlab="bair",ylab="fish size")  #should add info about the statistical info, median, max
+
+#confidence intervals 置信区间
+
+#2. find the dependence between the time size bait used 
